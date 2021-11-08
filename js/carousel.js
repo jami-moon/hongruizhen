420986cf64097fe495024f15d40a3985888fa53a
jami-moon <sherlockyu155@gmail.com>
2021-11-09 04:45:06 +0900

    ✨ Feat: 추가: .main-banner 관련 마크업 및 스타일링, 플러그인

---
+++ js/carousel.js
@@ -0,0 +1,13 @@
+const mainBannerCarousel = tns({
+  mode: "gallery",
+  container: ".banner .banner__list",
+  controls: false,
+  navContainer: ".banner .banner-nav__list",
+  navAsThumbnails: false,
+  arrowKeys: false,
+  autoplay: false,
+  autoplayHoverPause: false,
+  autoplayButtonOutput: false,
+  mouseDrag: false,
+  preventScrollOnTouch: true,
+});
